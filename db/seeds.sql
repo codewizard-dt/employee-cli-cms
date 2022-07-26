a59bc2bb3c5b7c58364d265723f108545fd6b4a0
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-07-26 17:44:53 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+USE employee_db;
+
+INSERT INTO departments (name)
+VALUES ("Engineering"),
+       ("Human Resources"),
+       ("Sales"),
+       ("Customer Service");
+       
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Engineering Lead", 100000, 1),
+       ("Front End Developer", 80000, 1),
+       ("Back End Developer", 90000, 1),
+       ("Full Stack Developer", 120000, 1),
+       ("Human Resources Lead", 100000, 2),
+       ("Benefits Manager", 60000, 2),
+       ("Hiring Manager", 70000, 2),
+       ("Sales Lead", 100000, 3),
+       ("Direct Salesperson", 30000, 3),
+       ("Hot Leads Generator", 50000, 3),
+       ("Customer Service Lead", 100000, 4),
+       ("Customer Specialist", 30000, 4),
+       ("Onboarding Specialist", 40000, 4);
+       
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("David", "Taylor", 1, null),
+       ("Keisha", "Johnson", 5, null),
+       ("Sayid", "Patel", 8, null),
+       ("Cecilia", "Sirrius", 11, null);
+       
